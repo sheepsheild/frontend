1e50714c817f84423388d54ef63e78266e6e5ad6
sheepsheild <54502848+sheepsheild@users.noreply.github.com>
2022-07-14 00:26:49 +0430

    return to main state

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { React } from 'react';
 function App() {
   return(
     <div>
-    <div class="">
+    <div class="room">
         <div class="middleContent">
             <div class="sharedScreen">Shared Screen</div>
             <div class="messagesPanel">
