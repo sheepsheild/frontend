ce9eb7b71457afaae4cee3d1f329a246089e3887
sheepsheild <54502848+sheepsheild@users.noreply.github.com>
2022-07-14 17:36:40 +0430

    return to base state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import { React, useState } from 'react';
+import { React } from 'react';
 
 function App() {
-    const [ShowMessagesPanel,setShowMessagesPanel]=useState(0);
-
   return(
     <div>
     <div class="room">
         <div class="middleContent">
             <div class="sharedScreen">Shared Screen</div>
-            <div class="messagesPanel" >
+            <div class="messagesPanel">
                 <h3 class="messagesHeader">messages</h3>
                 <div class="messages">
                     <ul>
@@ -48,7 +46,7 @@ function App() {
                 </div>
                 <div class="typeBox">
                     <p>Type here</p>
-                    <img alt="img" src="/icon/send.png" />
+                    <span>icon</span>
                 </div>
             </div>
         </div>
@@ -101,35 +99,7 @@ function App() {
             </div>
         </div>
         <span class="underline"></span>
-        <div class="footerPanel">
-            <p>class name</p>
-            <div class="CenterMenu">
-                <ul>
-                    <li>
-                        <img alt="icon" src="/icon/microphone.png" />
-                    </li>
-                    <li>
-                        <img alt="icon" src="/icon/video-camera.png" />
-                    </li>
-                    <li>
-                        <img alt="icon" src="/icon/raise-hand.png" />
-                    </li>
-                    <li>
-                        <img alt="icon" src="/icon/menu.png" />
-                    </li>
-                    <li>
-                        <img alt="icon" src="/icon/phone.png" />
-                    </li>
-                </ul>
-            </div>
-            <div class="RightMenu">
-                <ul>
-                    <li>
-                        <img alt="icon" src="/icon/chat.png"  onClick={()=> setShowMessagesPanel(!ShowMessagesPanel)}/>
-                    </li>
-                </ul>
-            </div>
-        </div>
+        
     </div>
   </div>
   )
