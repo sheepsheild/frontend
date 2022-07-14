a45dadfb6f2fbf66187a158b2bde8df931698a40
sajjad79f <fadayisajjad@gmail.com>
2022-07-14 15:57:44 +0430

    add icon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { React } from 'react';
+import { React, useState } from 'react';
 
 function App() {
+    const [ShowMessagesPanel,setShowMessagesPanel]=useState(0);
+
   return(
     <div>
     <div class="room">
         <div class="middleContent">
             <div class="sharedScreen">Shared Screen</div>
-            <div class="messagesPanel">
+            <div class="messagesPanel" >
                 <h3 class="messagesHeader">messages</h3>
                 <div class="messages">
                     <ul>
@@ -46,7 +48,7 @@ function App() {
                 </div>
                 <div class="typeBox">
                     <p>Type here</p>
-                    <span>icon</span>
+                    <img alt="img" src="/icon/send.png" />
                 </div>
             </div>
         </div>
@@ -99,7 +101,35 @@ function App() {
             </div>
         </div>
         <span class="underline"></span>
-        <div class="footerPanel"></div>
+        <div class="footerPanel">
+            <p>class name</p>
+            <div class="CenterMenu">
+                <ul>
+                    <li>
+                        <img alt="icon" src="/icon/microphone.png" />
+                    </li>
+                    <li>
+                        <img alt="icon" src="/icon/video-camera.png" />
+                    </li>
+                    <li>
+                        <img alt="icon" src="/icon/raise-hand.png" />
+                    </li>
+                    <li>
+                        <img alt="icon" src="/icon/menu.png" />
+                    </li>
+                    <li>
+                        <img alt="icon" src="/icon/phone.png" />
+                    </li>
+                </ul>
+            </div>
+            <div class="RightMenu">
+                <ul>
+                    <li>
+                        <img alt="icon" src="/icon/chat.png"  onClick={()=> setShowMessagesPanel(!ShowMessagesPanel)}/>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
   </div>
   )
